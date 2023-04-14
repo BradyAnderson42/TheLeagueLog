3c5565a548a4d708d0ee84bdffb25ffbcc611024
BradyAnderson42 <bradya2005@gmail.com>
2023-04-14 15:39:31 -0400

    Example of  send message

--- src/main/java/LeagueLog.java
+++ src/main/java/LeagueLog.java
@@ -1,12 +1,34 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.internal.JDAImpl;
 
+import java.util.Optional;
+
 public class LeagueLog {
-    JDA jda = JDABuilder.createDefault("Njc3NjcwMDc0NzE4NDg2NTMx.XkX40g.8UjlPEdZMukFgTeQMSc0H2IpFNg")
-            .addEventListeners(new MessageListener())
-            .build();
-    jda.awaitReady();
-    Channel leagueLog = jda.getPrivateChannelById("league-log");
+    private static final String TOKEN = "";
+
+    JDA jda;
+    TextChannel leagueLog;
+
+    public LeagueLog() {
+        jda = JDABuilder.createDefault(TOKEN)
+                .addEventListeners(new MessageListener())
+                .build();
+        try {
+            jda.awaitReady();
+        } catch (Exception e) {
+
+        }
+        Optional<TextChannel> leagueLogOptional = jda.getGuildsByName("Dylan", false).get(0).getTextChannels().stream().filter(textChannel -> textChannel.getName().equals("league-log")).findFirst();
+        if (leagueLogOptional.isPresent()) {
+            leagueLog = leagueLogOptional.get();
+        }
+        else {
+            System.exit(-1);
+        }
+        //leagueLog.sendMessage("Hello World!").queue();
+    }
+
 }
